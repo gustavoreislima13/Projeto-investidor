bbedce6adf5f36696d6cef1f9fd9484802394eef
gustavoreislima13 <129910259+gustavoreislima13@users.noreply.github.com>
2024-06-10 16:43:42 -0300

    teste

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,3 +9,15 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (email === 'reisanselmo7@gmail.com' && password === '123456') {
+    window.location.href = 'http://localhost/loja/index.php/produtos';
+  } else {
+    alert('Invalid email or password.');
+  }
+});
